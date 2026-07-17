61e22af51dbd862c1492e532a60377c769ca5539
Adam Szamalek <szamalekadam@gmail.com>
2026-07-17 19:06:37 +0200

    feat(app): add title bar

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,8 @@
 use gpui::{AppContext, Context, Entity, IntoElement, ParentElement, Render, Styled, Window};
-use gpui_component::h_flex;
+use gpui_component::{h_flex, v_flex};
 
 use crate::{
-    components::AppSidebar,
+    components::{AppSidebar, AppTitleBar},
     state::{AppScreen, AppState},
     views::{DashboardView, TestView},
 };
@@ -10,14 +10,20 @@ use crate::{
 pub struct MainApp {
     app_state: Entity<AppState>,
     sidebar: Entity<AppSidebar>,
+    title_bar: Entity<AppTitleBar>,
 }
 
 impl MainApp {
     pub fn new(cx: &mut Context<Self>) -> Self {
         let app_state = cx.new(|cx| AppState::new(cx));
         let sidebar = cx.new(|cx| AppSidebar::new(app_state.clone(), cx));
+        let title_bar = cx.new(|_| AppTitleBar::new(sidebar.clone()));
 
-        Self { app_state, sidebar }
+        Self {
+            app_state,
+            sidebar,
+            title_bar,
+        }
     }
 
     fn render_view(&self, cx: &mut Context<Self>) -> impl IntoElement {
@@ -36,9 +42,12 @@ impl MainApp {
 
 impl Render for MainApp {
     fn render(&mut self, _window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
-        h_flex()
-            .size_full()
-            .child(self.sidebar.clone())
-            .child(self.render_view(cx))
+        h_flex().size_full().child(self.sidebar.clone()).child(
+            v_flex()
+                .size_full()
+                .flex_1()
+                .child(self.title_bar.clone())
+                .child(self.render_view(cx)),
+        )
     }
 }
