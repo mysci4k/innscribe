eaba481773a901761b5b8814f0d26937fc6866ef
Adam Szamalek <szamalekadam@gmail.com>
2026-07-19 18:22:14 +0200

    fix(sidebar): rename navigation group to general

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -115,7 +115,7 @@ impl Render for AppSidebar {
                     }),
             )
             .child(
-                SidebarGroup::new("Navigation").child(
+                SidebarGroup::new("General").child(
                     SidebarMenu::new()
                         .child(self.nav_item("Dashboard", AppScreen::Dashboard, AppIcon::Map, cx))
                         .child(self.nav_item("Test", AppScreen::Test, AppIcon::Map, cx)),
