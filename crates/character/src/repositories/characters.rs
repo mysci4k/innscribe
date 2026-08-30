bcebd3b0dcf21321f67bf4709567ccf8f5a6affc
Adam Szamalek <szamalekadam@gmail.com>
2026-08-30 22:20:14 +0200

    feat(character): add archived_only filter to list method

--- crates/character/src/repositories/characters.rs
+++ crates/character/src/repositories/characters.rs
@@ -26,11 +26,19 @@ impl Characters {
         self.db.clone()
     }
 
-    pub async fn list(&self, search: Option<&str>, sort: CharacterSort) -> Result<Vec<Character>> {
-        let filter = Character::fields()
-            .deleted_at()
-            .is_none()
-            .and(Character::fields().archived_at().is_none());
+    pub async fn list(
+        &self,
+        search: Option<&str>,
+        sort: CharacterSort,
+        archived_only: bool,
+    ) -> Result<Vec<Character>> {
+        let mut filter = Character::fields().deleted_at().is_none();
+        if archived_only {
+            filter = filter.and(Character::fields().archived_at().is_some())
+        } else {
+            filter = filter.and(Character::fields().archived_at().is_none())
+        }
+
         let filter = match search.map(str::trim).filter(|q| !q.is_empty()) {
             Some(q) => filter.and(Character::fields().name().like(format!("%{}%", q))),
             None => filter,
