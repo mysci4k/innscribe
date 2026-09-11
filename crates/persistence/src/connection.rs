2eb8fd086d915458df69d31baa15050de55e21e5
Adam Szamalek <szamalekadam@gmail.com>
2026-09-11 16:58:28 +0200

    refactor(persistence): delegate default database path to settings module

--- crates/persistence/src/connection.rs
+++ crates/persistence/src/connection.rs
@@ -1,5 +1,5 @@
 use anyhow::{Result, anyhow};
-use directories::ProjectDirs;
+use settings::paths;
 use std::{
     fs,
     path::{Path, PathBuf},
@@ -23,21 +23,8 @@ pub async fn connect(db_path: impl AsRef<Path>, models: ModelSet) -> Result<Db> 
     Ok(db)
 }
 
-const PROJECT_QUALIFIER: &str = "app";
-const PROJECT_ORGANIZATION: &str = "innscribestudio";
-const PROJECT_APPLICATION: &str = "innscribe";
-
-const DATABASE_SUBDIR: &str = "database";
-const DATABASE_FILENAME: &str = "innscribe.db";
-
 pub fn default_path() -> PathBuf {
-    let proj_dirs = ProjectDirs::from(PROJECT_QUALIFIER, PROJECT_ORGANIZATION, PROJECT_APPLICATION)
-        .expect("Failed to get project directories");
-
-    proj_dirs
-        .data_local_dir()
-        .join(DATABASE_SUBDIR)
-        .join(DATABASE_FILENAME)
+    paths::database_path()
 }
 
 static MIGRATIONS: MigrationSet = embed_migrations!("../../toasty");
