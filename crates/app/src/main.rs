aef61124df0587f4fd8e306c78dcfffbce957920
Adam Szamalek <szamalekadam@gmail.com>
2026-09-19 21:57:02 +0200

    feat: add app assets to gpui application

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -2,13 +2,14 @@ mod app;
 mod logging;
 
 use gpui_kit::{AppContext, WindowOptions, component::Root};
+use shared::assets::AppAssets;
 
 use crate::app::MainApp;
 
 fn main() {
     logging::init();
 
-    let app = gpui_kit::application();
+    let app = gpui_kit::application().with_assets(AppAssets);
 
     app.run(move |cx| {
         gpui_kit::init(cx);
