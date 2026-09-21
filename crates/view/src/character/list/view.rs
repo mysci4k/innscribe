e422b9c4d8c57ee92adfbeb254700c0b9b6afcb2
Adam Szamalek <szamalekadam@gmail.com>
2026-09-21 13:36:53 +0200

    feat: add character list view

---
+++ crates/view/src/character/list/view.rs
@@ -0,0 +1,15 @@
+use gpui_kit::{Context, IntoElement, ParentElement, Render, Styled, Window, base::v_flex};
+
+pub struct CharacterListView;
+
+impl CharacterListView {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Render for CharacterListView {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        v_flex().text_3xl().child("Character List View")
+    }
+}
